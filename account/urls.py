0a6b5b89bb114e0467dfe47f6f14ad5d80336239
izamha <glcode40@gmail.com>
2022-12-31 06:20:47 +0200

    Renamed api_views.py to api.py

--- account/urls.py
+++ account/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
-from . import api_views
+from . import api
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
-    path("register/", api_views.UserRegisterView.as_view()),
-    path("email-verify/", api_views.VerifyEmail.as_view(), name="email-verify"),
+    path("register/", api.UserRegisterView.as_view()),
+    path("email-verify/", api.VerifyEmail.as_view(), name="email-verify"),
     path("login/", TokenObtainPairView.as_view()),  # Login, Create a jwt token
     path("token/refresh/", TokenRefreshView.as_view()),  # Refresh an expired token
-    path("auth/", api_views.AuthenticatedUserView.as_view()),  # Authenticated User
+    path("auth/", api.AuthenticatedUserView.as_view()),  # Authenticated User
     path(
         "user/<int:pk>/",
-        api_views.UserDetail.as_view(),
+        api.UserDetail.as_view(),
     ),  # CRUD operations on User
 ]
